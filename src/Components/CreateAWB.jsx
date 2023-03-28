2c1ce4f8c83084292e6218bb831dbb5b0bb5bf2a
bhagyashribadave98 <118594286+bhagyashribadave98@users.noreply.github.com>
2023-03-28 12:33:57 +0530

    Adding files

---
+++ src/Components/CreateAWB.jsx
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+//import { DateField, DatePicker, DemoContainer } from '@mui/x-date-pickers';
+  
+const CreateAWB = () => {
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>    
+    )
+}
+export default CreateAWB  
